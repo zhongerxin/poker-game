da54e1f65ee91f6ce11df49a17f6d9910d82749a
Twox <zhongxin123456@gmail.com>
2025-12-06 14:46:55 +0800

    fix: 修正拼写错误中的多余字符

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -58,7 +58,7 @@ function App() {
 					Edit <code>worker/index.ts</code> to change the name
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p>
+			<p className="read-the-docs">Click on sthe logos to learn more</p>
 		</>
 	);
 }
