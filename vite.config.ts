61dd38988a87af2d2a6dd4d7cb758673d48d8f6e
Twox <zhongxin123456@gmail.com>
2025-12-06 20:01:01 +0800

    feat: 添加 vite-plugin-singlefile 插件支持单文件打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-	plugins: [react(), cloudflare()],
+	plugins: [react(), cloudflare(),viteSingleFile()],
 	build: {
     	minify: false
   	}
